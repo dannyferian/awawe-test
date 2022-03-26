db87a36d673097041e1c2e88b8962b8adba32a36
danny ferian <danny.ferian@coinbit.id>
2022-03-26 14:52:33 +0700

    add new seeder endpoint

--- go.mod
+++ go.mod
@@ -2,10 +2,13 @@ module knative
 
 go 1.18
 
-require github.com/labstack/echo/v4 v4.7.2
+require (
+	github.com/google/uuid v1.3.0
+	github.com/labstack/echo v3.3.10+incompatible
+)
 
 require (
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -15,5 +18,4 @@ require (
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"flag"
+	"io"
 	"net/http"
 	"os"
+	"time"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/google/uuid"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
+
+const brokerURL = ""
+
+var (
+	CeId          = flag.String("Ce-Id", uuid.NewString(), "ce-id")
+	CeSpecversion = flag.String("Ce-Specversion", "1.0", "ce-specversion")
+	ContentType   = flag.String("Content-Type", "application/json", "content-type")
+)
+
+type requestBody struct {
+	Message string `json:"message"`
+}
 
 func main() {
 
@@ -23,10 +42,49 @@ func main() {
 		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
 	})
 
+	e.POST("/seed", func(c echo.Context) error {
+		ctx, cancel := context.WithTimeout(context.TODO(), 5*time.Second)
+		defer cancel()
+
+		var requestBody = new(requestBody)
+		if err := c.Bind(&requestBody); err != nil {
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+
+		req, err := http.NewRequestWithContext(ctx, http.MethodPost, brokerURL, getParsedRequestBody(requestBody))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+
+		headers := map[string]string{
+			"Ce-Id":          *CeId,
+			"Ce-Specversion": *CeSpecversion,
+			"Ce-Type":        c.Request().Header.Get("Ce-Type"),
+			"Ce-Source":      c.Request().Header.Get("Ce-Source"),
+			"Content-Type":   *ContentType,
+		}
+
+		for key, val := range headers {
+			req.Header.Add(key, val)
+		}
+
+		if _, err := http.DefaultClient.Do(req); err != nil {
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.JSON(http.StatusAccepted, "seed sent successfully")
+	})
+
 	httpPort := os.Getenv("HTTP_PORT")
 	if httpPort == "" {
 		httpPort = "8080"
 	}
 
 	e.Logger.Fatal(e.Start(":" + httpPort))
+
+}
+
+func getParsedRequestBody(req *seedRequest) io.Reader {
+	reqData, _ := json.Marshal(req)
+	return bytes.NewBuffer(reqData)
 }
